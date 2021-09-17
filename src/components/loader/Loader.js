5963d86bb53ac66666ec9c92371f3bb680fe86a1
vice76 <arpityadavaj1999@gmail.com>
2021-09-17 19:12:38 +0530

    implemented loader

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <>
+      <div class="loader loader1">
+        <div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
