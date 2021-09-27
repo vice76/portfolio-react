0fee6d2c119107dfe2b7939b40f4abc87884f787
vice76 <arpityadavaj1999@gmail.com>
2021-09-27 22:57:27 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Cursor from "./components/cursor/Cursor";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
     <div className="App">
       <Cursor />
+      <Navbar />
     </div>
   );
 };
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+import logo from "../../assests/Arpit Yadav.png";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="nav__left">
+        <img src={logo} alt="Arpit Yadav" />
+        <a href="#">work.</a>
+        <a href="#">skills.</a>
+        <a href="#">say hi!</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
