816b6eef5d20b5b5568eb28d19e3729209a3266d
vice76 <arpityadavaj1999@gmail.com>
2021-09-28 00:58:49 +0530

    added text and svg ,no styling

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -3,7 +3,29 @@ import React from "react";
 const Landingpage = () => {
   return (
     <div className="mainpage">
-      <div className="mainpage_left"></div>
+      <div className="mainpage_left">
+        <h1>Non-Awarded Web Developer</h1>
+        <h4>But, hey no one cares about that.</h4>
+        <div className="meet">
+          <span>👇</span>
+          <p>Meet Arpit Yadav</p>
+        </div>
+        <svg width="63" height="104" viewBox="0 0 133 194" fill="none">
+          <rect
+            x="44"
+            y="38"
+            width="62"
+            height="127"
+            rx="31"
+            fill="#5B8A72"
+            fill-opacity="0.9"
+            stroke="white"
+            stroke-width="8"
+            stroke-linejoin="bevel"
+          />
+          <circle cx="75" cy="76" r="19" fill="white" />
+        </svg>
+      </div>
     </div>
   );
 };
