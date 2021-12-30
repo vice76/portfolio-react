64e34cdb6b63a3923f8ec26e24318b4e8f45c8d6
vice76 <arpityadavaj1999@gmail.com>
2021-12-30 19:53:19 +0530

    added text and fixed ui

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -5,13 +5,19 @@ const Landingpage = () => {
   return (
     <div className="mainpage">
       <div className="mainpage_left">
-        <h1>Non-Awarded Web Developer</h1>
-        <h4>But, hey no one cares about that.</h4>
-        <div className="meet">
-          <span>👇</span>
-          <p>Meet Arpit Yadav</p>
+        <div className="mainpage_content">
+          <h1>Non-Awarded </h1>
+          <h1>Web</h1>
+          <h1>Developer</h1>
+          <h4>But, hey no one cares about that.</h4>
+          <div className="meet">
+            <span>👇</span>
+            <p id="name">Meet Arpit Yadav</p>
+          </div>
+          <div className="scrolldown">
+            <ScrollDown />
+          </div>
         </div>
-        <ScrollDown />
       </div>
     </div>
   );
